48524201edb7b4d2713b4bdd1a9c9c93cf5fc3c1
aneesyounus <aneesyounus@gmail.com>
2022-09-27 16:37:14 +0500

    updated

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+// function scripts_method() {
+// 	wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery' ) );
+// }
+// add_action( 'wp_enqueue_scripts', 'scripts_method' );
+
+?>
